ee8c0b5595c1c1376cc84b038db42df4f6d5d459
Max Patsiansky <m.patsianskiy@zmeke.com>
2015-01-23 18:04:05 +0300

    Storing the Game Map in Memory

---
+++ src/js/Walls.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function Walls() {
+  PIXI.DisplayObjectContainer.call(this);
+
+  this.pool = new WallSpritesPool();
+  this.createLookupTables();
+}
+
+Walls.constructor = Walls;
+Walls.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
+
+Walls.prototype.createLookupTables = function() {
+  this.borrowWallSpriteLookup = [];
+  this.borrowWallSpriteLookup[SliceType.FRONT] = this.pool.borrowFrontEdge;
+  this.borrowWallSpriteLookup[SliceType.BACK] = this.pool.borrowBackEdge;
+  this.borrowWallSpriteLookup[SliceType.STEP] = this.pool.borrowStep;
+  this.borrowWallSpriteLookup[SliceType.DECORATION] = this.pool.borrowDecoration;
+  this.borrowWallSpriteLookup[SliceType.WINDOW] = this.pool.borrowWindow;
+
+  this.returnWallSpriteLookup = [];
+  this.returnWallSpriteLookup[SliceType.FRONT] = this.pool.returnFrontEdge;
+  this.returnWallSpriteLookup[SliceType.BACK] = this.pool.returnBackEdge;
+  this.returnWallSpriteLookup[SliceType.STEP] = this.pool.returnStep;
+  this.returnWallSpriteLookup[SliceType.DECORATION] = this.pool.returnDecoration;
+  this.returnWallSpriteLookup[SliceType.WINDOW] = this.pool.returnWindow;
+};
+
+Walls.prototype.borrowWallSprite = function(sliceType) {
+  return this.borrowWallSpriteLookup[sliceType].call(this.pool);
+};
+
+Walls.prototype.returnWallSprite = function(sliceType, sliceSprite) {
+  return this.returnWallSpriteLookup[sliceType].call(this.pool, sliceSprite);
+};
